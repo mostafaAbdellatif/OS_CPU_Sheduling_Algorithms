6b1f234e182e4da97ba9f080d3d8f757df1e958a
Ahmed1208 <ahmedhoss14@yahoo.com>
2020-04-01 04:49:50 +0200

    shortest job first2

--- src/sample/RoundRobin/RRChartController.java
+++ src/sample/RoundRobin/RRChartController.java
@@ -6,11 +6,13 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import sample.publicVariables;
 
-
-
 public class RRChartController {
 
     publicVariables publicVariablesObject = new publicVariables();
+
+    static Button past;
+    static int lastProcess;
+    static int width;
 
     @FXML
     private HBox gantchart;
@@ -25,14 +27,11 @@ public class RRChartController {
 
         System.out.println(publicVariablesObject.processData);
 
-        publicVariablesObject.ordering(publicVariablesObject.start);
+        publicVariablesObject.ordering(publicVariablesObject.start); //order according to start time
 
         System.out.println(publicVariablesObject.processData);
 
-        //publicVariablesObject.indexingProcess();
-
         System.out.println(publicVariablesObject.processData);
-
 
         while (!publicVariablesObject.processData.isEmpty()) {
             int s=publicVariablesObject.processData.size();
@@ -41,8 +40,14 @@ public class RRChartController {
                 int m = publicVariablesObject.processData.get(x).get(1) - publicVariablesObject.quantum;
                 if(publicVariablesObject.processData.get(x).get(1)>0)
                 {
-                    Button H = publicVariablesObject.makeButton(publicVariablesObject.processData.get(x).get(2), publicVariablesObject.quantum);
-                    gantchart.getChildren().add(H);
+                    if(m>=0){
+                        formButton(x,publicVariablesObject.quantum);
+                    }
+                    else
+                    {
+                        formButton(x,publicVariablesObject.processData.get(x).get(1));
+                    }
+
                 }
                 publicVariablesObject.processData.get(x).set(1, m);
              //  if (m <= 0)
@@ -66,5 +71,24 @@ public class RRChartController {
 
         }
 
+    }
+
+    public void formButton(int x,int wid) {
+        if(lastProcess != publicVariablesObject.processData.get(x).get(2))  //if last process not equal this process
+        {
+            Button H = publicVariablesObject.makeButton(publicVariablesObject.processData.get(x).get(2), wid );
+            past = H;
+            lastProcess = publicVariablesObject.processData.get(x).get(2);
+            width = wid;
+            gantchart.getChildren().add(H);
+        }
+        else
+        {
+            width += wid;
+            gantchart.getChildren().remove(past);
+            Button H = publicVariablesObject.makeButton(lastProcess,width);
+            past = H;
+            gantchart.getChildren().add(H);
+        }
     }
 }
--- src/sample/ShortJobFirst/SJFChartController.java
+++ src/sample/ShortJobFirst/SJFChartController.java
@@ -5,11 +5,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import sample.publicVariables;
+
+import java.awt.geom.NoninvertibleTransformException;
 import java.util.ArrayList;
 
 public class SJFChartController {
 
     publicVariables publicVariablesObject = new publicVariables();
+
+    ArrayList<ArrayList<Integer>> workProcess = new ArrayList(20);
+
+    static Button past;
+    static int lastProcess;
+    static int width;
 
     @FXML
     private HBox gantchart;
@@ -17,6 +25,7 @@ public class SJFChartController {
     private Label averageTime;
 
     public void initialize() {
+
 
 // clear previous data from prossessData
         publicVariablesObject.processData.clear();
@@ -45,55 +54,125 @@ public class SJFChartController {
         }
 
         //////////////////////Permative//////////////////////////////
-        else if (publicVariablesObject.permative == Boolean.TRUE) {
-//1- order all processes according to arrival time
-// (if 2 have same arrival time then order accordding to shortest burst)
+        else {
+
+//1- order acoording to start yo burst
             publicVariablesObject.orderingStartThenBurst();
+//2- save timeline as a counter according to first arrival time
+            int timeLine = publicVariablesObject.processData.get(0).get(0);
 
             System.out.println(publicVariablesObject.processData);
 
-//2- make arraylist to store difference between arrival times (x - (x-1))
-            ArrayList<Integer> diff = new ArrayList();
+            int size = publicVariablesObject.processData.size();
+            int maxArrival = publicVariablesObject.processData.get(size - 1).get(0);
+            int minArrival = publicVariablesObject.processData.get(0).get(0);
+//3- loop till all arrival times are done
+            for (int t = minArrival; t <= maxArrival; t++) {
+//4- if timeline equals to arrival time of process, add it to arraylist(workprocess)
+                if (publicVariablesObject.starts.contains(timeLine)) {
+                    for (int x = 0; x < size; x++) {
+                        if (timeLine == publicVariablesObject.processData.get(x).get(0)) {
+                            workProcess.add(publicVariablesObject.processData.get(x));
+                        }
+                    }
+                }
+//5- order processes add to workprocess acording to burst time only
+                ordering(1, workProcess);
+//6- remove 1 from burst time of first process in workProsses
+                timeLine = removeOneBurst(timeLine);
 
-            for (int x = 0; x < publicVariablesObject.starts.size() - 1; x++) {
-                int m = publicVariablesObject.starts.get(x + 1) - publicVariablesObject.starts.get(x);
-                diff.add(m);
-            }
-
-            for (int x = 0; x < diff.size(); x++) {
-
-//3- subtract process's burst time - first diff, so you can stop process and take second process
-                int m1 = publicVariablesObject.processData.get(0).get(1) - diff.get(x);
-//4- make button for process
-                if (publicVariablesObject.processData.get(0).get(1) > 0) {
-                    Button H = publicVariablesObject.makeButton(publicVariablesObject.processData.get(0).get(2), diff.get(x));
+//7- check if this process was also past process,if -->then mix 2 buttons together
+                if (lastProcess != workProcess.get(0).get(2)) {
+                    Button H = publicVariablesObject.makeButton(workProcess.get(0).get(2), 1);
+                    past = H;
+                    lastProcess = workProcess.get(0).get(2);
+                    width = 1;
+                    gantchart.getChildren().add(H);
+                } else {
+                    width += 1;
+                    gantchart.getChildren().remove(past);
+                    Button H = publicVariablesObject.makeButton(lastProcess, width);
+                    past = H;
                     gantchart.getChildren().add(H);
                 }
 
-//5- set new burst time
-                publicVariablesObject.processData.get(0).set(1, m1);
-
-//6- order again according to first process and add one only (not all processes)
-//ordering( according to burst , number of elements you need to compare)
-                publicVariablesObject.ordering(publicVariablesObject.burst, (2 + x));
-
-                System.out.println(publicVariablesObject.processData);
+                System.out.println(workProcess);
+//8- delete process has zero burst time
+                zeroBurstRemover();
             }
+//9- continue all processes as non-permative as all processes get ordered according to bursts
+            int wpSize = workProcess.size();
+            for (int x = 0; x < wpSize; x++) {
 
-//7- all processes are added so no more arrival times
-//8- make buttons as non-permative according to shortest burst
-            for (int x = 0; x < publicVariablesObject.processData.size(); x++) {
-                Button H = publicVariablesObject.makeButton(publicVariablesObject.processData.get(x).get(2), publicVariablesObject.processData.get(x).get(1));
-                gantchart.getChildren().add(H);
+                formButton(x);
 
-                int m = publicVariablesObject.processData.get(x).get(1) - publicVariablesObject.processData.get(x).get(1);
-                publicVariablesObject.processData.get(x).set(1, m);
+                int m = workProcess.get(x).get(1) - workProcess.get(x).get(1);
+                workProcess.get(x).set(1, m);
+                System.out.println(workProcess);
+
             }
-
-            System.out.println(publicVariablesObject.processData);
 
         }
-
     }
 
+    public void zeroBurstRemover(){
+        int size = workProcess.size();
+        for(int x=0;x<size;x++)
+        {
+            try {
+                if (workProcess.get(x).get(1) == 0)
+                {
+                    workProcess.remove(x);
+                }
+                }catch (Exception e) {
+                    continue;
+            }
+        }
+    }
+
+    public int removeOneBurst(int timeLine){
+        int m = workProcess.get(0).get(1) - 1 ;
+        workProcess.get(0).set(1, m);
+        timeLine +=1;
+        return timeLine;
+    }
+
+    public void ordering(int startORburst,ArrayList<ArrayList<Integer>> wp){
+        int s = wp.size();
+        for (int i = 0; i < s; i++) {
+            for (int j = 1; j < (s - i); j++) {
+                if (wp.get(j - 1).get(startORburst) > wp.get(j).get(startORburst)) {
+                    ArrayList<ArrayList<Integer>> l = new ArrayList();
+                    l.clear();
+                    l.add(wp.get(j - 1));
+                    wp.remove(j - 1);
+
+                    try {
+                        wp.add(j, l.get(0));
+                    } catch (Exception e) {
+                        wp.set(j, l.get(0));
+                    }
+                }
+            }
+        }
+    }
+
+    public void formButton(int x) {
+        if(lastProcess != workProcess.get(x).get(2))  //if last process not equal this process
+        {
+            Button H = publicVariablesObject.makeButton(workProcess.get(x).get(2), workProcess.get(x).get(1));
+            past = H;
+            lastProcess = workProcess.get(x).get(2);
+            width = workProcess.get(x).get(1);
+            gantchart.getChildren().add(H);
+        }
+        else
+        {
+            width += workProcess.get(x).get(1);
+            gantchart.getChildren().remove(past);
+            Button H = publicVariablesObject.makeButton(lastProcess,width);
+            past = H;
+            gantchart.getChildren().add(H);
+        }
+    }
 }
